24456daa73923d0ae9e7e2ecfb1951d5cbcda557
Patrick Selge <patrick.selge@daparto.com>
2021-12-24 20:52:17 +0100

    add ssg story

---
+++ pages/ssg/story.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <Blok :blok="story.content" />
+    </div>
+</template>
+
+<script>
+import { useStoryblock, addVisualEditorEventListener } from "~/lib/storyblok";
+
+export default {
+    data() {
+        return {
+            data: { content: {}}
+        }
+    },
+    async asyncData(ctx) {
+        const story = await useStoryblock(ctx);
+
+        return {
+            story
+        }
+    },
+    mounted() {
+        addVisualEditorEventListener(this.$storybridge, this.story)
+    }
+}
+</script>
