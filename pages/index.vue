90b7d06cfddc45d1bbc0b992149aacae8b15933f
Patrick Selge <patrick.selge@daparto.com>
2021-12-24 20:56:05 +0100

    add story pages to the index file

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,13 @@
             <dd>Fetches the current time from http://worldtimeapi.org and renders it's result on each request as a server side response.</dd>
             <dt><a href="/ssg/time">🚀 SSG Time ›</a></dt>
             <dd>Fetches the current time during build from http://worldtimeapi.org and renders it's results from a static html file.</dd>
+
+            <dt><a href="/ssr/story">🐌 SSR Story ›</a></dt>
+            <dd>Fetches a story from storyblock and renders it on each request as a server side response.</dd>
+            <dt><a href="/ssg/story">🚀 SSG Story ›</a></dt>
+            <dd>Fetches a story during build from storyblock and renders it's results from a static html file.</dd>
+            <dt><a href="/ssr/cached/story">🚀 Cached SSR Story ›</a></dt>
+            <dd>Fetches a story from storyblock and renders it on each request as a server side response. Uses an in memory cache identified by the deployed commit sha.</dd>
         </dl>
     </div>
 </template>
