6b7a6fa2d75bccc15937a4804b18145bb9b8d68c
Patrick Selge <patrick.selge@daparto.com>
2021-12-24 19:21:08 +0100

    initial commit

---
+++ components/Time.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="time">
+        {{ time }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        time: {
+            required: true,
+        }
+    }
+}
+</script>
---
+++ pages/ssg/time.vue
@@ -0,0 +1,15 @@
+<template>
+    <Time :time="time" />
+</template>
+
+<script>
+export default {
+    async asyncData() {
+        const { data: { datetime: time } } = await $axios.get("http://worldtimeapi.org/api/timezone/Europe/Berlin")
+
+        return {
+            time
+        }
+    }
+}
+</script>
