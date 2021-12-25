a64124dd7bf13e2ce901cee53f2233b8cdda2c0f
Patrick Selge <patrick.selge@daparto.com>
2021-12-25 10:27:28 +0100

    add sleep, to show speed difference

--- pages/ssr/cdn/time.vue
+++ pages/ssr/cdn/time.vue
@@ -4,12 +4,16 @@
 
 <script>
 import cacheControl from "~/helpers/cacheControl"
+import sleep from "~/lib/sleep"
+
 export default {
     middleware: cacheControl({
         's-maxage': 3,
     }),
     async asyncData({ $axios }) {
         const { data: { datetime: time } } = await $axios.get("http://worldtimeapi.org/api/timezone/Europe/Berlin")
+
+        await sleep(1500)
 
         return {
             time
