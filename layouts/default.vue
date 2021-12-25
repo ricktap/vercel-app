7e8456c54875a3b1cb9004ece0ae1033a191dc18
Patrick Selge <patrick.selge@daparto.com>
2021-12-25 10:58:22 +0100

    add header, for easier navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="container">
+        <nav class="top">
+            <h1><a href="/">Vercel-App</a></h1>
+        </nav>
         <Nuxt />
     </div>
 </template>
@@ -17,5 +20,24 @@
 
     align-items: center;
     justify-content: center;
+}
+
+nav {
+    width: 100%;
+    background-color: black;
+    color: white;
+    font-family: sans-serif;
+    padding: 8px 10px;
+}
+
+nav a {
+    color: white;
+    text-decoration: none;
+}
+
+nav.top {
+    position: fixed;
+    top: 0;
+    left: 0;
 }
 </style>
